463c2697154ed20be0094827bcbcbf67c4f11356
TaylorWu <173890875@qq.com>
2016-02-26 15:02:07 +0800

    cascade setter test

---
+++ B.java
@@ -0,0 +1,16 @@
+package com.zf.qqcy.dataService.common.excel;
+
+/**
+ * Created by Taylor on 2016/2/26.
+ */
+public class B {
+    C c;
+
+    public C getC() {
+        return c;
+    }
+
+    public void setC(C c) {
+        this.c = c;
+    }
+}
